b4d3b173d362f4e3aa25085527fbc6ec2c054994
cros6y6raga <crosbybraga@gmail.com>
2023-03-05 13:49:36 +0300

    HW-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -12,10 +12,14 @@ function Clock() {
     const start = () => {
         const id: number = +setInterval(() => {
             setDate(new Date())
+            // setDate
         }, 1000)
         setTimerId(id)
+        //     const d = new Date()
+        //  return document.getElementById('timeID')
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+
     }
 
     const stop = () => {
@@ -25,18 +29,21 @@ function Clock() {
     }
 
     const onMouseEnter = () => {
-
         setShow(true)// пишут студенты // показать дату если наведена мышка
+
     }
     const onMouseLeave = () => {
+        setShow(false)// пишут студенты // спрятать дату если мышка не наведена
 
-        setShow(false)// пишут студенты // показать дату если наведена мышка
     }
 
     const stringTime = date?.toLocaleTimeString('ru-RU') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date?.toLocaleDateString('ru-RU') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
+    // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturdays'];
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+    // const stringDay = days[new Date().getDay()] || <br/>; // пишут студенты
+    // const stringMonth = months[new Date().getMonth()] || <br/> // пишут студенты
     const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date) || <br/>
     const stringMonth = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date) || <br/>
 
@@ -48,7 +55,7 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
+                <span id={'hw9-day'}>{stringDay}</span>{' '}
                 <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
@@ -58,8 +65,9 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
+
+                            <span id={'hw9-date'}>{stringDate}</span>,{' '}
+                            <span id={'hw9-month'}>{stringMonth}</span>
                         </>
                     ) : (
                         <>
@@ -68,11 +76,11 @@ function Clock() {
                     )}
                 </div>
             </div>
-
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
                     disabled={!!timerId}
+                    // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
