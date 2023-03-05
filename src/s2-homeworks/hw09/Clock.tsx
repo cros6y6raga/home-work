da659f965cd726295eba6ecce714f55882b81783
cros6y6raga <crosbybraga@gmail.com>
2023-03-05 13:47:32 +0300

    const stringDay

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -37,7 +37,7 @@ function Clock() {
     const stringDate = date?.toLocaleDateString('ru-RU') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date) || <br/>
     const stringMonth = 'date->month' || <br/> // пишут студенты
 
     return (
