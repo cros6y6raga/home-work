54b6a218eac202dc97fd2dc8198b60aa78419e47
cros6y6raga <crosbybraga@gmail.com>
2023-03-08 10:51:19 +0300

    Fix

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -10,16 +10,10 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        const id: number = +setInterval(() => {
-            setDate(new Date())
-            // setDate
-        }, 1000)
+        const id: number = +setInterval(() => {setDate(new Date())}, 1000)
         setTimerId(id)
-        //     const d = new Date()
-        //  return document.getElementById('timeID')
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
     }
 
     const stop = () => {
@@ -30,23 +24,15 @@ function Clock() {
 
     const onMouseEnter = () => {
         setShow(true)// пишут студенты // показать дату если наведена мышка
-
     }
     const onMouseLeave = () => {
         setShow(false)// пишут студенты // спрятать дату если мышка не наведена
-
     }
 
     const stringTime = date?.toLocaleTimeString('ru-RU') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date?.toLocaleDateString('ru-RU') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-    // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    // const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturdays'];
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    // const stringDay = days[new Date().getDay()] || <br/>; // пишут студенты
-    // const stringMonth = months[new Date().getMonth()] || <br/> // пишут студенты
     const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date) || <br/>
     const stringMonth = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date) || <br/>
-
     return (
         <div className={s.clock}>
             <div
