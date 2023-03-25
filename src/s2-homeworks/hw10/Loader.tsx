f0d19a00502b11cb06eeba31ad04765f7f1caa4b
cros6y6raga <crosbybraga@gmail.com>
2023-03-25 16:18:04 +0300

    Loader

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
 import s from './Loader.module.css'
+import spinner from './spinner.gif'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () =>
+    <div className={s.loader} ><img src={spinner}/></div>
