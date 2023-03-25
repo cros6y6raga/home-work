ea8b763ab9dddbc539383a86745c91cfef082733
cros6y6raga <crosbybraga@gmail.com>
2023-03-25 16:18:40 +0300

    loading

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,8 +2,14 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState=> { // fix any
     switch (action.type) {
+        case 'CHANGE_LOADING': {
+            return{
+                ...state,
+                isLoading: action.isLoading
+            }
+        }
         // пишет студент  // need to fix
 
         default:
