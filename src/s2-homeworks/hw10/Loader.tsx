dc63763ed004876e754ba6a01ea6416fe6db7717
cros6y6raga <crosbybraga@gmail.com>
2023-03-25 16:23:37 +0300

    Spinner and Ref

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -2,4 +2,4 @@ import s from './Loader.module.css'
 import spinner from './spinner.gif'
 
 export const Loader = () =>
-    <div className={s.loader} ><img src={spinner}/></div>
+    <div className={s.loader}><img src={spinner}/></div>
